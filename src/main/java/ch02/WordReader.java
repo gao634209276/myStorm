b33df9e8aafdc155b68fd8f4a80b5396752d81be
noah <634209276@qq.com>
2017-04-21 10:33:16 +0800

    read

--- src/main/java/ch02/WordReader.java
+++ src/main/java/ch02/WordReader.java
@@ -21,20 +21,29 @@ public class WordReader extends BaseRichSpout {
 	private FileReader fileReader;
 	private boolean completed = false;
 
-	public void ack(Object msgId) {
-		System.out.println("OK:" + msgId);
-	}
-
-	public void close() {
-	}
-
-	public void fail(Object msgId) {
-		System.out.println("FAIL:" + msgId);
+	/**
+	 * 第一个被调用
+	 * 我们将创建一个文件并维持一个collector对象
+	 *
+	 * @param conf 配置对象，在定义topology对象是创建
+	 * @param context TopologyContext对象，包含所有拓扑数据
+	 * @param collector 还有SpoutOutputCollector对象，它能让我们发布交给bolts处理的数据
+	 */
+	@Override
+	public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
+		try {
+			//创建了一个FileReader对象，用来读取文件
+			this.fileReader = new FileReader(conf.get("wordsFile").toString());
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException("Error reading file [" + conf.get("wordFile") + "]");
+		}
+		this.collector = collector;
 	}
 
 	/**
 	 * 这个方法做的惟一一件事情就是分发文件中的文本行
 	 */
+	@Override
 	public void nextTuple() {
 		/**
 		 *  这个方法会不断的被调用，直到整个文件都读完了，我们将等待并返回。
@@ -65,17 +74,22 @@ public class WordReader extends BaseRichSpout {
 		}
 	}
 
-	/**
-	 * 我们将创建一个文件并维持一个collector对象
-	 */
-	public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
-		try {
-			this.fileReader = new FileReader(conf.get("wordsFile").toString());
-		} catch (FileNotFoundException e) {
-			throw new RuntimeException("Error reading file [" + conf.get("wordFile") + "]");
-		}
-		this.collector = collector;
+
+	@Override
+	public void ack(Object msgId) {
+		System.out.println("OK:" + msgId);
 	}
+
+	@Override
+	public void close() {
+	}
+
+	@Override
+	public void fail(Object msgId) {
+		System.out.println("FAIL:" + msgId);
+	}
+
+
 
 	/**
 	 * 声明输入域"word"
